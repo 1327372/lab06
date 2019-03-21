1da7fb6359978ae4499ad1b8a62db70e5fa139b9
1327372 <48769525+1327372@users.noreply.github.com>
2019-03-21 17:43:17 -0400

    Update 1327372_lab6_q2.java

--- 1327372_lab6_q2.java
+++ 1327372_lab6_q2.java
@@ -2,6 +2,7 @@ package 1327372_lab6;
 import java.util.Scanner;
 public class 1327372_lab6_q2 {
 
+	
 	// Copying me gets us both zero. Just use this as inspiration.
 	
 	public static void main(String[] args) {
